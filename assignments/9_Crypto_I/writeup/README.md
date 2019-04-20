595f7ab3a0bf0a856dc2197d6d934f2fca57c54a
William Guo <wguo1@terpmail.umd.edu>
2019-04-20 18:00:02 -0400

    submit assignment 9

--- assignments/9_Crypto_I/writeup/README.md
+++ assignments/9_Crypto_I/writeup/README.md
@@ -1,15 +1,20 @@
 # Crypto I Writeup
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: William Guo
+Section: 0101
 
 I pledge on my honor that I have not given or received any unauthorized
 assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: William Guo
 
 ## Assignment Writeup
 
 ### Part 1 (100 Pts)
 
-*Your reflection goes here*
+I thought this project was pretty simplistic. Obviously this would be useless in the real
+world since we could assume that the password was only salted with a single character.
+Though it was pretty straightforward for this project. Simply appending a character to the
+password in plaintext and then hashing that and comparing it with the hashes that we do know.
+Then outputing it. There really wasn't anything difficult about this assignment. Really cool
+concepts though using SHA256 and just the concept of hashing in general. 
--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -3,17 +3,25 @@
 import hashlib
 import string
 
-def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
-    characters = string.ascii_lowercase
+def crack(): #cracks passwords
+	hashes = []
+	passwords = []
+	with open ('hashes.txt') as file: #reads in the hashes
+		hashes = file.readlines()
+	hashes = list(map(str.strip, hashes)) #removes whitespace and newline from each input
+	
+	with open ('passwords.txt') as file: #reads in the passwords
+		passwords = file.readlines()
+	passwords = list(map(str.strip, passwords)) #removes whitespace and newline from each input
+    
+	characters = string.ascii_lowercase #gets all lowercase chars in the english alphabet
 
-    for c in characters:
-        for p in passwords:
-            # crack hashes
+	for p in passwords: #iterates through all passwords
+		for c in characters: #iterates through all chars
+			h = hashlib.sha256(c + p) #gets the SHA256 hash from appending the char to the passoword
+			if (h.hexdigest() in hashes): #sees if it is in the hash list
+				print(c + p + ": " + h.hexdigest()) #prints output
+				break #exits inner loop since it was already found
 
-            # print hashes as 'input:hash'
-            # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
-
-if __name__ == "__main__":
+if __name__ == "__main__": #main method, calls the crack method
     crack()
