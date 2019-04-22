fc333e42ce0481ca104d116234609669005f8392
guowil <williamguo18702@hotmail.com>
2019-04-22 12:14:00 -0400

    push edits for crypto 1

--- assignments/9_Crypto_I/writeup/README.md
+++ assignments/9_Crypto_I/writeup/README.md
@@ -11,12 +11,16 @@ Digital acknowledgement: William Guo
 ## Assignment Writeup
 
 ### Part 1 (70 Pts)
-
-### Part 2 (30 Pts)
-
 I thought this project was pretty simplistic. Obviously this would be useless in the real
 world since we could assume that the password was only salted with a single character.
 Though it was pretty straightforward for this project. Simply appending a character to the
 password in plaintext and then hashing that and comparing it with the hashes that we do know.
 Then outputing it. There really wasn't anything difficult about this assignment. Really cool
-concepts though using SHA256 and just the concept of hashing in general. 
+concepts though using SHA256 and just the concept of hashing in general.
+### Part 2 (30 Pts)
+This part was pretty straightforward too. I did have trouble comparing strings though before I
+noticed that the output included a bunch of stuff other than the hash. Other than that I took a
+different approach this time by using a dictionary and essentially creating a hashmap of all of the
+hashes with those as keys and the passwords as the values. The flag was CMSC389R-{h@sh1ng_@nd_sl@sh1ng}.
+
+ 
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -5,18 +5,33 @@ import string
 import socket
 import time
 
-def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
-    characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+def server_crack(): #cracks passwords from servers
+	passwords = []
+	hashes = {}
+	
+	with open ('passwords.txt') as file: #reads in the passwords
+		passwords = file.readlines()
+	passwords = list(map(str.strip, passwords)) #removes whitespace and newline from each input
+    
+	characters = string.ascii_lowercase #gets all lowercase chars in the english alphabet
+	server_ip = "134.209.128.58"
+	server_port = 1337
 
-    s = socket.socket(socket.AF_INET, socket.STREAM)
-    s.connect((server_ip, server_port))
-    data = s.recv(1024)
-    # parse data
-    # crack 3 times
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((server_ip, server_port))
+	
+	for p in passwords: #iterates through all passwords
+		for c in characters: #iterates through all chars
+			h = hashlib.sha256(c + p) #gets the SHA256 hash from appending the char to the passoword
+			hashes[h.hexdigest()] = c + p #maps the key as the hash and the value as the password
+	
+	for x in range(3): #iterate three times
+		data = s.recv(1024).split("\n")[2] #parses the output and get the hash value
+		if (data in hashes): #check if hash is in the dictionary
+			print (hashes[data] + "\n") 
+			s.send(hashes[data] + "\n") #send it to the server
+			
+	print s.recv(1024) #gets the flag
 
-if __name__ == "__main__":
+if __name__ == "__main__": #main method, calls the server crack method
     server_crack()
